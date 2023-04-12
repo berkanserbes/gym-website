d4b308cafc32f8d26cc4070bdb5cef60f731b483
Berkan Serbes <berkan_serbes@hotmail.com>
2023-04-12 19:10:58 +0300

    Modified About.js

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,23 @@
 import React from "react";
+import aboutimg from "../assets/images/about.png";
 
 function About() {
-  return <div>About</div>;
+  return (
+    <div id="about">
+      <div className="about-image">
+        <img src={aboutimg} alt="" />
+      </div>
+      <div className="about-text">
+        <h1>LEARN MORE ABOUT US</h1>
+        <p>
+          Lorem ipsum,Lorem ipsum,Lorem ipsum,Lorem ipsum,Lorem ipsum,Lorem
+          ipsum,Lorem ipsum,Lorem ipsum,Lorem ipsum,Lorem ipsum,Lorem
+          ipsum,Lorem ipsum,Lorem ipsum,Lorem ipsum,Lorem ipsum,Lorem
+        </p>
+        <button>READ MORE</button>
+      </div>
+    </div>
+  );
 }
 
 export default About;
