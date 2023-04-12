d97e8d780b7a50e85f9712015cca7bf188e4966c
Berkan Serbes <berkan_serbes@hotmail.com>
2023-04-12 19:11:36 +0300

    Modified Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,24 @@
 import React from "react";
 
 function Header() {
-  return <div>Header</div>;
+  return (
+    <div id="main">
+      <div className="name">
+        <h2>STEP UP YOUR</h2>
+        <h1>
+          <span>FITNESS</span>WITH US
+        </h1>
+        <p className="details">
+          Build Your Body and Fitness With Professional Touch
+        </p>
+        <div className="header-btns">
+          <a href="#main" className="header-btn">
+            JOIN US
+          </a>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Header;
