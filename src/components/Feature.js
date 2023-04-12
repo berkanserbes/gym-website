71f84901ec369acdff8884b67a16117862d7ed2b
Berkan Serbes <berkan_serbes@hotmail.com>
2023-04-12 20:32:09 +0300

    Modified Feature.js

--- src/components/Feature.js
+++ src/components/Feature.js
@@ -10,7 +10,7 @@ function Feature() {
     <div id="features">
       <h1>FEATURES</h1>
       <div className="a-container">
-        <Featurebox image={fimage1} title="WeightLifting" />
+        <Featurebox image={fimage1} title="Weight Lifting" />
         <Featurebox image={fimage2} title="Specific Muscle" />
         <Featurebox image={fimage3} title="Flex Your Muscle" />
         <Featurebox image={fimage4} title="Cardio Exercise" />
